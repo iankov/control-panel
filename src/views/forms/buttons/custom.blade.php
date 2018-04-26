092cdb7dcb8dd14a91c6a6cf36843dda58e5c01f
Dmitriy <iankovd@gmail.com>
2018-04-26 19:34:44 +0300

    show and custom buttons added

---
+++ src/views/forms/buttons/custom.blade.php
@@ -0,0 +1,3 @@
+<button data-method="{{$method or 'GET'}}" data-action="{{$action or ''}}" type="button" class="btn btn-{{$size or 'xs'}} {{$class or 'btn-default'}}">
+    <i class="fa fa-{{$icon or 'hand-o-up'}}" data-toggle="tooltip" data-placement="top" title="{{$title or ''}}" data-original-title="{{$title or ''}}"></i>
+</button>
---
+++ src/views/forms/buttons/show.blade.php
@@ -0,0 +1,3 @@
+<button data-method="GET" data-action="{{$action}}" type="button" class="btn btn-xs btn-primary">
+    <i class="fa fa-eye" data-toggle="tooltip" data-placement="top" title="Show" data-original-title="Show"></i>
+</button>
